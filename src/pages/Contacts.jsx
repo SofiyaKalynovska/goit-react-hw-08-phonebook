7c7bd569a17572c1b3d4e87a5864b1aa863bc096
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-29 20:38:06 +0200

    Create page for contscts

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,45 @@
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Section from '../components/Section/Section';
+import Filter from './Filter/Filter';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectIsLoading, selectError } from 'redux/selectors';
+import { ThreeDots } from 'react-loader-spinner';
+import { Container } from './App.styled';
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <Container>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        {isLoading && !error ? (
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#33415c"
+            ariaLabel="three-dots-loading"
+            wrapperClassName=""
+            visible={isLoading}
+            wrapperStyle={{ display: 'inline' }}
+          />
+        ) : (
+          <Filter />
+        )}
+        <ContactList />
+      </Section>
+    </Container>
+  );
+};
