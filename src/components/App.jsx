388b148de80d2b93a4f2a8e98a615f0fecab400b
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-30 18:00:29 +0200

    Add App and hook useAuth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,16 @@
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Section from '../components/Section/Section';
-import Filter from './Filter/Filter';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from 'redux/operations';
-import { selectIsLoading, selectError } from 'redux/selectors';
-import { ThreeDots } from 'react-loader-spinner';
-import { Container } from './App.styled';
+import { lazy } from "react";
+import { Routes, Route } from 'react-router-dom';
+
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LogInPage = lazy(() => import('../pages/LogIn'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
-    <Container>
-      <Section title="Phonebook">
-        <ContactForm />
-      </Section>
-      <Section title="Contacts">
-        <Filter />
-        {isLoading && !error && (
-          <ThreeDots
-            height="80"
-            width="80"
-            radius="9"
-            color="#33415c"
-            ariaLabel="three-dots-loading"
-            wrapperStyle={{}}
-            wrapperClassName=""
-            visible={isLoading}
-          />
-        )}
-        <ContactList />
-      </Section>
-    </Container>
+    <Routes>
+      <Route path="/" element={<ContactsPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LogInPage />} />
+    </Routes>
   );
-};
+}
