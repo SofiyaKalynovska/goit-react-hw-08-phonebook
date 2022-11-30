b58498702a80bd9dfae493761c7458a0e912e5cf
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-30 19:14:21 +0200

    Update selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,3 +2,9 @@ export const selectContacts = state => state.contacts.contacts;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter.filter;
+
+export const selectIsLoggedIn = state => state.contacts.isLoggedIn;
+
+export const selectUser = state => state.contacts.user;
+
+export const selectIsRefreshing = state => state.contacts.isRefreshing;
