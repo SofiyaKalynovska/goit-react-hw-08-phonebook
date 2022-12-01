b1150d2579e10cb6fa05b6a1ca4060feef0789d1
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-01 18:30:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import { lazy } from "react";
+import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Layout } from './Layout';
 
 const RegisterPage = lazy(() => import('../pages/Register'));
 const LogInPage = lazy(() => import('../pages/LogIn'));
@@ -8,9 +9,11 @@ const ContactsPage = lazy(() => import('../pages/Contacts'));
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<ContactsPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LogInPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ContactsPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LogInPage />} />
+      </Route>
     </Routes>
   );
-}
+};
