33d8bb91666f9d456586934314eddf69b7fff3b5
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-02 19:05:58 +0200

    Add logOut extraReducer

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,15 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { register, logIn } from "./authOperations";
+import { register, logIn, logOut } from "./authOperations";
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState: {
     user: { name: null, email: null },
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
   },
-  extraReducers: builder => {
+  extraReducers: builder => 
     builder.addCase(register.pending, (state) => state).addCase(register.fulfilled, (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
@@ -18,8 +18,12 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
-})
-  }
+    }).addCase(logOut.fulfilled, state => {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    })
+  
 })
 
 export const authReducer = authSlice.reducer;
