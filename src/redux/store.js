3aeef2f81142215f5fb54a6b747d7b2838df9f52
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-04 23:26:37 +0200

    Add auth to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,28 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { authReducer } from "./authSlice";
+import storage from 'redux-persist/lib/storage';
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
+import { authReducer } from './authSlice';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, REGISTER, PURGE, PAUSE, PERSIST } from 'redux-persist';
 
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filtersReducer,
-  auth: authReducer,
-});
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+    filter: filtersReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+export const persistor = persistStore(store);
