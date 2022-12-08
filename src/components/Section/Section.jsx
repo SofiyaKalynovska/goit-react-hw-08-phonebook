445e3f313784f2f00f9e52f9bf7f4d4f56c2f38f
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-08 23:51:58 +0200

    Add MUI styles to Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,12 +1,23 @@
 import PropTypes from 'prop-types';
-import { SectionBox, SectionTitle } from './Section.styled';
+import { Box, Typography } from '@mui/material';
 
 export default function Section({ title, children }) {
   return (
-    <SectionBox>
-      <SectionTitle>{title}</SectionTitle>
+    <Box
+      sx={{
+        backgroundColor: '#e8ebff',
+        marginTop: '30px',
+        boxShadow:
+          ' rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;',
+        padding: 2,
+        borderRadius: 3,
+      }}
+    >
+      <Typography variant="h4" component="h2">
+        {title}
+      </Typography>
       {children}
-    </SectionBox>
+    </Box>
   );
 }
 
