ff7fded7d4d08880dbddf57337cfda936d8924b2
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-08 23:04:23 +0200

    Update Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,14 @@
 import { RegisterForm } from '../components/RegisterForm/RegisterForm';
+import { Helmet } from 'react-helmet';
+import { Container } from '@mui/material';
 
 export default function Register() {
   return (
-    <div>
-      <title>Registration</title>
+    <Container component="main" maxWidth="sm" sx={{ pt: 12 }}>
+      <Helmet>
+        <title>Registration</title>
+      </Helmet>
       <RegisterForm />
-    </div>
+    </Container>
   );
 }
