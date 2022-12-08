7fc5c81137cb9eba58b9b5fc8e29e03f298f23cf
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-08 22:31:10 +0200

    Add MUI styles to RegisterForm

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Form = styled.form`
+width: 90%;
 display: flex;
 flex-direction: column;
 gap: 20px;
+font-size: 26px;
 
 `
 // export const InputName = styled.label`
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,10 @@
 import { useDispatch } from 'react-redux';
 import { register } from '../../redux/authOperations';
-import { RegisterFormStyled, RegisterFormLabel, RegisterFormInput } from './RegisterForm.styled';
+// import { RegisterFormStyled, RegisterFormLabel, RegisterFormInput } from './RegisterForm.styled';
+import { PersonAddAlt1TwoTone } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
+import { Container, Box, Typography, TextField, Button } from '@mui/material';
+import { Form } from 'components/ContactForm/ContactForm.styled';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -19,20 +23,66 @@ export const RegisterForm = () => {
   };
 
   return (
-    <RegisterFormStyled onSubmit={handleSubmit} autoComplete="off">
-      <RegisterFormLabel>
-        Username
-        <RegisterFormInput type="text" name="name" />
-      </RegisterFormLabel>
-      <RegisterFormLabel >
-        Email
-        <RegisterFormInput type="email" name="email" />
-      </RegisterFormLabel>
-      <RegisterFormLabel >
-        Password
-        <RegisterFormInput type="password" name="password" />
-      </RegisterFormLabel>
-      <button type="submit">Register</button>
-    </RegisterFormStyled>
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          backgroundColor: '#e8ebff',
+          marginTop: '30px',
+          border: '3px solid #1976d2',
+          padding: 2,
+          borderRadius: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar alt="Register icon" sx={{ m: 1 }}>
+          <PersonAddAlt1TwoTone />
+        </Avatar>
+        <Typography component="h1" variant="h4" color="#1976d2">
+          Sign in
+        </Typography>
+        <Form onSubmit={handleSubmit} autoComplete="off">
+          <Typography variant="body" component="label" color="#1976d2">
+            Username
+          </Typography>
+          <TextField
+            fullWidth="true"
+            label="Provide name"
+            variant="outlined"
+            type="text"
+            fontSize="40"
+            name="name"
+            required
+            inputProps={{ style: { fontSize: 24 } }}
+          />
+          <Typography variant="body" component="label" color="#1976d2">
+            Email
+          </Typography>
+          <TextField
+            label="Provide password"
+            variant="outlined"
+            type="email"
+            name="email"
+            required
+            inputProps={{ style: { fontSize: 24 } }}
+          />
+          <Typography variant="body" component="label" color="#1976d2">
+            Password
+          </Typography>
+          <TextField
+            label="Provide password"
+            variant="outlined"
+            type="password"
+            name="password"
+            required
+            inputProps={{ style: { fontSize: 24 } }}
+          />
+          <Button variant="contained" size="large" color="info" type="submit">
+            Register
+          </Button>
+        </Form>
+      </Box>
+    </Container>
   );
 };
