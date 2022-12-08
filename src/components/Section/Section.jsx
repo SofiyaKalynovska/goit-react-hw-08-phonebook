b931cc7a3dfc1e0d0dd12f44e06b95a8f14cf239
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-08 23:55:57 +0200

    Add MUI styles to Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,23 +1,23 @@
 import PropTypes from 'prop-types';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Container } from '@mui/material';
 
 export default function Section({ title, children }) {
   return (
-    <Box
-      sx={{
-        backgroundColor: '#e8ebff',
-        marginTop: '30px',
-        boxShadow:
-          ' rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;',
-        padding: 2,
-        borderRadius: 3,
-      }}
-    >
-      <Typography variant="h4" component="h2">
-        {title}
-      </Typography>
-      {children}
-    </Box>
+    <Container sx={{mt: 3}}>
+      <Box
+        sx={{
+          backgroundColor: '#e8ebff',
+          boxShadow:
+            ' rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;',
+          padding: 2,
+        }}
+      >
+        <Typography variant="h4" component="h2">
+          {title}
+        </Typography>
+        {children}
+      </Box>
+    </Container>
   );
 }
 
