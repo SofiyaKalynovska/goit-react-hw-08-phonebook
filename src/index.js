fd1f27da4bac2c0806f82b3a585c5e1ada907ddb
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-09 22:33:44 +0200

    Add basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ import { store, persistor } from 'redux/store'
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from 'redux-persist/integration/react';
 import { AppWrapper } from 'index.styled';
-// import { Wrapper } from '../src/components/App.styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter >
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <AppWrapper>
             <App />
           </AppWrapper>
@@ -22,4 +21,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
-// basename = "/goit-react-hw-08-phonebook"
