7c737799655b4125f6f98af57d61a98e8b70ecc1
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-09 22:22:06 +0200

    Add Mui styles to Filter

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -4,14 +4,4 @@ import { Container } from '@mui/material';
 export const Wrapper = styled(Container)`
   padding: 20px;
   margin: auto;
-  width: 300px;
-  @media (min-width: 480px) {
-    width: 420px;
-  }
-  @media (min-width: 768px) {
-    width: 700px;
-  }
-  @media (min-width: 1280px) {
-    width: 1000px;
-  }
 `;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,9 @@
-import { FilterTitle, FilterInput } from './Filter.styled';
+// import { FilterTitle, FilterInput } from './Filter.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectFilter } from 'redux/selectors';
 import { changeFilter } from 'redux/filtersSlice';
+import { Typography, TextField, Box } from '@mui/material';
+import { Form } from 'components/ContactForm/ContactForm.styled';
 
 const Filter = () => {
   const filter = useSelector(selectFilter);
@@ -11,15 +13,39 @@ const Filter = () => {
     dispatch(changeFilter(e.currentTarget.value));
   };
   return (
-    <>
-      <FilterTitle>Find contacts by name</FilterTitle>
-      <FilterInput
+    <Box
+      sx={{
+        backgroundColor: '#e8ebff',
+        margin: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Form>
+        <Typography variant="body" component="label" color="#1976d2">
+          Find contacts by name
+        </Typography>
+        {/* <FilterTitle>Find contacts by name</FilterTitle> */}
+        <TextField
+          fullWidth="true"
+          label="Start typing..."
+          variant="outlined"
+          type="search"
+          fontSize="40"
+          name="filter"
+          value={filter}
+          onChange={handleFilter}
+          inputProps={{ style: { fontSize: 20 } }}
+        />
+        {/* <FilterInput
         type="search"
         name="filter"
         value={filter}
         onChange={handleFilter}
-      />
-    </>
+      /> */}
+      </Form>
+    </Box>
   );
 };
 
