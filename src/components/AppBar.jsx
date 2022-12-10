198049740a95b84fc3cc28a571e1248dd5fbb4f3
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-10 21:12:31 +0200

    Update AppBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,14 +3,28 @@ import { UserMenu } from './UserMenu';
 import { AuthNav } from './AuthNav';
 import { useAuth } from '../hooks/useAuth';
 import { Navigation } from './Navigation';
+import { Box } from '@mui/material';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <div>
+    <Box
+      as="header"
+      p={2}
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: '#1976d2',
+        alignItems: 'center',
+        borderRadius: 2,
+
+        background:
+          'linear-gradient(180deg, rgba(3,39,102,1) 0%, rgba(25,118,210,1) 71%, rgba(0,157,255,1)100%)',
+      }}
+    >
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </div>
+    </Box>
   );
 };
