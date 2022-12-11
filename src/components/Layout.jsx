67cdaab013a05316660309c3807c7217d798f17b
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-11 19:34:10 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,16 @@ import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AppBar } from './AppBar';
 import { Suspense } from 'react';
+import { Box } from '@mui/material';
 
 export const Layout = () => {
   return (
-    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+    <Box sx={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
       <AppBar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
       <Toaster position="top-right" reverseOrder={false} />
-    </div>
+    </Box>
   );
 };
