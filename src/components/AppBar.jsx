82ec68836b11b827672682c107c4a41c214a4111
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-12 18:00:30 +0200

    Add styles to Navigation

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -18,7 +18,6 @@ export const AppBar = () => {
         backgroundColor: '#1976d2',
         alignItems: 'center',
         borderRadius: 2,
-
         background:
           'linear-gradient(180deg, rgba(3,39,102,1) 0%, rgba(25,118,210,1) 71%, rgba(0,157,255,1)100%)',
       }}
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,59 @@
-import { Link } from 'react-router-dom';
+import { Button } from '../../node_modules/@mui/material/index';
+import { NavLink } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
-
+import { Box } from '@mui/material';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
   return (
-    <nav>
-      {isLoggedIn ?
-        <Link to="/contacts">
-          Contacts
-        </Link>
-       : <Link to="/">Start</Link>}
-      
-    </nav>
+    <Box>
+      {isLoggedIn ? (
+        <Button
+          size="small"
+          variant="contained"
+          sx={{
+            maxHeight: '25px',
+            fontWeight: 'bold',
+            border: '1px solid white',
+            backgroundColor: '#f7e9c4',
+            borderRadius: 1,
+            padding: '5px',
+          }}
+        >
+          <NavLink
+            to="/contacts"
+            style={{
+              textDecoration: 'none',
+              color: 'red',
+            }}
+          >
+            Contacts
+          </NavLink>
+        </Button>
+      ) : (
+        <Button
+          size="small"
+          variant="contained"
+          sx={{
+            maxHeight: '25px',
+            fontWeight: 'bold',
+            border: '1px solid white',
+            backgroundColor: '#f7e9c4',
+            borderRadius: 1,
+            padding: '5px',
+          }}
+        >
+          <NavLink
+            to="/"
+            style={{
+              textDecoration: 'none',
+              color: 'red',
+            }}
+          >
+            Start
+          </NavLink>
+        </Button>
+      )}
+    </Box>
   );
 };
