eb024dd60c173a05a6f69cc32631c6dc6cdaf491
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-12-12 19:21:05 +0200

    Add styles to AuthNav

--- src/components/AuthNav.jsx
+++ src/components/AuthNav.jsx
@@ -1,14 +1,65 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { Button, Box, Typography } from '@mui/material';
 
 export const AuthNav = () => {
   return (
-    <div>
-      <Link to="/register">
-        Register
-      </Link>
-      <Link to="/login">
-        Log In
-      </Link>
-    </div>
+    <Box 
+      sx={{
+        m: {xs: "5px", sm: "0px"},
+        display: 'flex',
+        gap: { xs: '5px', sm: '20px' },
+        alignItems: 'center',
+        flexDirection: { xs: 'column', sm: 'inherit' },
+      }}
+    >
+      <Button
+        size="small"
+        variant="contained"
+        sx={{
+          fontWeight: 'bold',
+          border: '1px solid white',
+          backgroundColor: '#f7e9c4',
+          borderRadius: 1,
+          padding: '5px',
+          ':focus-within': { backgroundColor: '#cbba83' },
+        }}
+      >
+        <NavLink style={{ textDecoration: 'none' }} to="/register">
+          <Typography
+            sx={{
+              color: '#1976d2',
+              ':hover': { color: 'red' },
+              fontWeight: 'bold',
+            }}
+          >
+            Register
+          </Typography>
+        </NavLink>
+      </Button>
+      <Button
+        size="small"
+        variant="contained"
+        sx={{
+          fontWeight: 'bold',
+          border: '1px solid white',
+          backgroundColor: '#f7e9c4',
+          borderRadius: 1,
+          padding: '5px',
+          ':focus-within': { backgroundColor: '#cbba83' },
+        }}
+      >
+        <NavLink style={{ textDecoration: 'none' }} to="/login">
+          <Typography
+            sx={{
+              color: '#1976d2',
+              ':hover': { color: 'red' },
+              fontWeight: 'bold',
+            }}
+          >
+            Log in
+          </Typography>
+        </NavLink>
+      </Button>
+    </Box>
   );
 };
